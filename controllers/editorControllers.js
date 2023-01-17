8e70ec6a1e3324d8ab3e3fe9685276e803c72755
DJehSantana <jessicah.dias23@hotmail.com>
2023-01-17 01:44:47 -0300

    Inserido métodos get e post no controller editor

--- controllers/editorControllers.js
+++ controllers/editorControllers.js
@@ -1,5 +1,13 @@
-async function getAllEditors(req, res) {
+import { db } from '../database/dbConfig.js'
 
+async function getAllEditors(req, res) {
+    try {
+        db.all('select * from editor', (err, rows) => {
+            res.json(rows)
+        });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
 }
 
 async function getEditor(req, res) {
@@ -7,6 +15,20 @@ async function getEditor(req, res) {
 }
 
 async function postEditor(req, res) {
+    try {
+        const { name, id_book } = req.body;
+
+        if (!name || !id_book) {
+            throw new Error('Favor preencher campos obrigatórios');
+        }
+
+        const stmt = db.prepare('insert into editor (name, id_book) values(?, ?)');
+        stmt.run(name, id_book);
+        res.json({ name, id_book });
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
 
 }
 
